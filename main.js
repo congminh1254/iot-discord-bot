e9f2d75af2fe5901effff760b7976eeda280a512
congminh1254 <clovers1254@gmail.com>
2020-12-07 23:09:08 +0100

    Update webserver

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
-
+const http = require('http');
 
 const discordClient = new Discord.Client();
+
 
 discordClient.on('ready', () => {
 	console.log(`Logged in as ${discordClient.user.tag}!`);
@@ -14,3 +15,9 @@ discordClient.on('message', (msg) => {
 });
 
 discordClient.login(process.env.DISCORD_BOT_KEY);
+
+
+http.createServer(function (req, res) {
+	res.write('Hi there, I\'m running!');
+	res.end();
+}).listen(80);
