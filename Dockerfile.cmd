00c0ef66e945fba8944a94dabf953b7c98bad95b
Minh Nguyen Cong <congminh1254@idrac-esx1.net.ds.pw.edu.pl>
2022-07-30 13:47:51 +0200

    Update config directory

--- Dockerfile.cmd
+++ Dockerfile.cmd
@@ -1,4 +1,4 @@
 docker stop iot-discord-bot && docker rm iot-discord-bot
 docker build . -t iot/iot-discord-bot
-docker run -d -it --name iot-discord-bot --restart always -p 8080:8080 --env-file /root/config/iot-discord-bot/.env iot/iot-discord-bot
+docker run -d -it --name iot-discord-bot --restart always -p 8080:8080 --env-file /root/OneDrive/config/iot-discord-bot/.env iot/iot-discord-bot
 docker image prune -f
